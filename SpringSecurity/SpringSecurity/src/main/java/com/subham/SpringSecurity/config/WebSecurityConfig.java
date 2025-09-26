66f1ebc7cb0a102ec2fe5f5ef344e3890cd2334a
Subham-0 <subhamd2022@gift.edu.in>
2025-09-26 22:15:37 +0530

    adding costume configuration

---
+++ SpringSecurity/SpringSecurity/src/main/java/com/subham/SpringSecurity/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.subham.SpringSecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+//                .csrf(csrf->csrf.disable())
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+}
