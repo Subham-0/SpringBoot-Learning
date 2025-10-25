4472fb172ead013aaf6d2f7e1b5ca854f61fcba2
Subham-0 <subhamd2022@gift.edu.in>
2025-10-25 18:38:06 +0530

    feat: implement complete JWT authentication flow with token generation, validation, and filtering

--- SpringSecurity-2/SpringSecurity-2/src/main/java/com/subham/SpringSecurity_2/config/SecurityConfig.java
+++ SpringSecurity-2/SpringSecurity-2/src/main/java/com/subham/SpringSecurity_2/config/SecurityConfig.java
@@ -16,13 +16,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
-    MyUserDetailsService myUserDetailsService;
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -34,6 +38,7 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
